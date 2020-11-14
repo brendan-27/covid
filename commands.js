dc2c6a0a15f0edad7b0a99749d3001e2c80ae87d
whatland <47169269+whatland@users.noreply.github.com>
2020-11-14 11:52:30 -0800

    2.2.6.2

--- commands.js
+++ commands.js
@@ -152,7 +152,7 @@ bot.on("message", async msg=>  {
             .setTimestamp()
             .setFooter("COVID-19 Bot | 2.2 | ")
         } catch {
-            console.log(newDeaths);
+            console.log(newRecovs);
             Embed = new discord.MessageEmbed()
             .setColor(colors.blue)
             .setAuthor("Error:", bot.user.displayAvatarURL())
